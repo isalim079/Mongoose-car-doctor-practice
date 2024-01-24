e4b5950778aedfafd733b9b93362651620773aa0
isalim079 <salimreza1025@gmail.com>
2024-01-24 12:54:57 +0600

    first commit

---
+++ car-doctor-server/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT ||5000
+
+
+app.get('/health', (req, res) => {
+    res.send('doctor is running')
+})
+
+app.all("*", (req, res, next) => {
+    const error = new Error(`The requested url is invalid: [${req.url}]`)
+    error.status = 400
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+
+    res.status(err.status || 500).json({
+        message:err.message
+    })
+
+    // console.log('from line 18');
+})
+
+app.listen(port , () => {
+    console.log(`car doctor server is running on port ${port}`);
+})
