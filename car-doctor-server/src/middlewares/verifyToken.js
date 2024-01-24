fd6ef37722b770b800e5aab88fc74c715f04611d
isalim079 <salimreza1025@gmail.com>
2024-01-24 15:47:06 +0600

    mongoose connection to mongodb done

--- car-doctor-server/src/middlewares/verifyToken.js
+++ car-doctor-server/src/middlewares/verifyToken.js
@@ -1,3 +1,4 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 
 const verifyToken = (req, res, next) => {
@@ -9,7 +10,7 @@ const verifyToken = (req, res, next) => {
     if(!token) {
         return res.status(401).send({message: 'unauthorized access'})
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    JsonWebTokenError.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) {
             return res.status(401).send({message: "unauthorized access"})
         }
